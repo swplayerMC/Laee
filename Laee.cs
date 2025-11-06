d607a28b9c7d524605d3a15c38c27103059f8a87
Calebe <swplayermc30@gmail.com>
2025-11-06 17:06:42 -0300

    Choosing a comment style

---
+++ Laee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Laee
+{
+  class Laee
+  {
+    public static void Run(string laeCode)
+    {
+      // string functionPattern = @"(^|\s+)(\w+)\(\)";
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Laee
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* Comments are every text after first '--' and before line end, like:
+       * -- This is a comment
+       * or anything between '--[' and ']--' like
+       * --[
+       *  this is a multi-line comment
+       * ]--
+       * And the line should end after ']--' but it's not obligatory. */
+      //string comments = @"(--\[[^\]\-\-]*\]--)|(--[^\n]*)"; // MultiLine --[]--
+      //string comments = @"(--\[\[[^\]\]]*\]])|(--[^\n]*)"; // MultiLine --[[]]
+      string comments = @"|(?:--[^.]*)"; // MultiLine --{}
+      string laeCode = args.Length > 0 ? File.ReadAllText(args[0]) : "";
+      // Regex re;
+      Console.WriteLine(Regex.Replace(laeCode, comments, string.Empty));
+    }
+  }
+}
