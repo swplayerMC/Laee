6823c93ae48dd71db8786998e2077d707c7d8592
Calebe <swplayermc30@gmail.com>
2025-11-08 13:10:00 -0300

    What if i program it in C or C++?

---
+++ Laee++/lae.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  char message[] = "Hello, world!";
+  printf(message);
+  return 0;
+}
---
+++ Laee++/lae.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char message[] = "Hello world in a variable!\n";
+  cout << message;
+  return 0;
+}
