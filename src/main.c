17f4ace76a2dd949b9550880a8d3cd349891207b
Calebe <swplayermc30@gmail.com>
2025-11-11 16:45:48 -0300

    Almost getting text file contents

--- src/main.c
+++ src/main.c
@@ -3,5 +3,17 @@
 int main() {
   char msg[] = "Lae\tversion 0.0.1-dev\n";
   printf("%s", msg);
+    FILE *p_file = fopen("/home/calebe/Dev/Lae/examples/variables.lae", "r");
+  char buffer[1024] = {0};
+
+  if(p_file == NULL) {
+    printf("Could not open file\n");
+    return 1;
+  }
+
+  while (fgets(buffer, sizeof(buffer), p_file) != NULL) {
+    printf("%s", buffer); 
+  }
+  fclose(p_file);
   return 0;
 }
