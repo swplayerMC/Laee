38d75cf5762a134315e3c66be8b6884441c98884
Calebe <swplayermc30@gmail.com>
2025-11-12 17:02:48 -0300

    Added: 'help' command

--- src/main.c
+++ src/main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-//#include <string.h>
+#include <string.h>
+
+void print_usage_guide() {
+      printf("usage:\t[file] [options] or\n\t[command] [options]\n\t");
+}
 
 int main(int argc, char *argv[]) {
   char version[] = "0.0.1-dev";
-  FILE *pFile = fopen(argv[1], "r");
+  FILE *p_file = fopen(argv[1], "r");
   char buffer[1024] = {0};
 
-  if(pFile == NULL) {
+  if(p_file == NULL) {
     if (argc == 1) {
       printf("Lae\tversion %s\nRun 'lae help' to see the usage guide\n", version);
       return 0;
     }
+    else if (strncmp(argv[1], "help", 4) == 0) {
+      print_usage_guide();
+      return 0;
+    }
+
     printf("Could not open file\n");
     return 1;
   }
 
-  while (fgets(buffer, sizeof(buffer), pFile) != NULL) {
-    printf("%s", buffer);
-  }
-  fclose(pFile);
-  return 0;
+    while (fgets(buffer, sizeof(buffer), p_file) != NULL) {
+	printf("%s", buffer);
+    }
+    fclose(p_file);
+    return 0;
 }
