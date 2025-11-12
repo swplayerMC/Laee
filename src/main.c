42487e6fcb13cf1ba7de694b83fedd4eee225d52
Calebe Souza <swplayermc30@gmail.com>
2025-11-12 14:01:26 -0300

    Now it can receive arguments

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+//#include <string.h>
 
-int main() {
-  char msg[] = "Lae\tversion 0.0.1-dev\n";
-  printf("%s", msg);
-    FILE *p_file = fopen("/home/calebe/Dev/Lae/examples/variables.lae", "r");
+int main(int argc, char *argv[]) {
+  char version[] = "0.0.1-dev";
+  FILE *pFile = fopen(argv[1], "r");
   char buffer[1024] = {0};
 
-  if(p_file == NULL) {
+  if(pFile == NULL) {
+    if (argc == 1) {
+      printf("Lae\tversion %s\nRun 'lae help' to see the usage guide\n", version);
+      return 0;
+    }
     printf("Could not open file\n");
     return 1;
   }
 
-  while (fgets(buffer, sizeof(buffer), p_file) != NULL) {
-    printf("%s", buffer); 
+  while (fgets(buffer, sizeof(buffer), pFile) != NULL) {
+    printf("%s", buffer);
   }
-  fclose(p_file);
+  fclose(pFile);
   return 0;
 }
